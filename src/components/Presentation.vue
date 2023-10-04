e7de02869556e574c27c27a00bd57e15500df32c
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-10-04 11:36:27 +0200

    rectification lien cv

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -48,7 +48,9 @@ import PresentationItemSkills from "./PresentationItemSkills.vue";
         </div>
         <div class="container-button">
           <button>
-            <a href="/Portfolio/src/assets/CV-FCHAMBINAUD.pdf" target="blank"
+            <a
+              href="https://lebrasdejesus.github.io/Portfolio/src/assets/CV-FCHAMBINAUD.pdf"
+              target="blank"
               >Télécharger mon cv</a
             >
           </button>
